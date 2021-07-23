c3539afeda4f070485b6f9c886ebdb87d65ae0cd
Gabriel <moraesnicol@gmail.com>
2021-07-23 18:36:58 -0300

    finish

---
+++ Super-Senha/SecurityTipsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SecurityTipsViewController.swift
+//  Super-Senha
+//
+//  Created by Gabriel on 22/07/21.
+//
+
+import UIKit
+
+class SecurityTipsViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func close(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
--- Super-Senha/ViewController.swift
+++ Super-Senha/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tfTotalPasswords: UITextField!
+    @IBOutlet weak var tfNumberOfCharacters: UITextField!
+    @IBOutlet weak var swLetters: UISwitch!
+    @IBOutlet weak var swNumbers: UISwitch!
+    @IBOutlet weak var swCapitalLetters: UISwitch!
+    
+    @IBOutlet weak var swSpecialCharacteres: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let passwordsViewController = segue.destination as! PasswordsViewController
+        if let numbertOfPasswords = Int(tfTotalPasswords.text!) {
+            passwordsViewController.numberOfPasswords = numbertOfPasswords
+        }
+        if let numbertOfCharacters = Int(tfTotalPasswords.text!) {
+            passwordsViewController.numberOfPasswords = numbertOfCharacters
+    }
+        passwordsViewController.useLetters = swLetters.isOn
+        passwordsViewController.useNumbers = swNumbers.isOn
+        passwordsViewController.useCapitalLetters = swCapitalLetters.isOn
+        passwordsViewController.useSpecialCharacters = swSpecialCharacteres.isOn
+        view.endEditing(true)
 }
 
+}
